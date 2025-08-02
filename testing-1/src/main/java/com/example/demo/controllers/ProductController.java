591abb1e77efa35d7dc47f09cfb1a105e8c7f763
abhishek975 <dixitabhi1605@gmail.com>
2025-08-02 13:49:46 +0530

    Final Changes

--- testing-1/src/main/java/com/example/demo/controllers/ProductController.java
+++ testing-1/src/main/java/com/example/demo/controllers/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.entities.Product;
@@ -16,6 +17,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/app")
+@EnableWebMvc
 public class ProductController {
    @Autowired
 	private ProductService  productService;
